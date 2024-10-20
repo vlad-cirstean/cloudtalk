5a8901eb916df86ccbb32904bdf9fc23e0c31c9a
Vlad Cirstean <vlad.cirstean@twipla.com>
2024-10-20 16:32:09 +0300

    feat: add openapi

--- product-svc/src/main.ts
+++ product-svc/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+      .setTitle('Product API')
+      .setDescription('Service for managing products and reviews')
+      .setVersion('1.0')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.startAllMicroservices();
   await app.listen(configService.get('tcp.port'));
